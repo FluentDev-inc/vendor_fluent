52a090f4e2ea76a9a68c83b8ea822c061e4f0856
Rafael Pacheco Méndez <rafy.pachecomendez@gmail.com>
2022-01-09 14:26:16 -0300

    rebase to don't use the patches from Ponces

--- config/common.mk
+++ config/common.mk
@@ -27,9 +27,6 @@ ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
 else
-# Enable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
-
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
